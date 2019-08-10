23009b11643db408de0cf5e973aa2dce400bcf02
Jason Loomis <jason@jrloomis.com>
2019-08-10 14:08:01 -0400

    save the films

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -21,10 +21,10 @@ const Movie = props => {
   }, [props.match.params.id]);
 
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie);
+  };
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -34,7 +34,9 @@ const Movie = props => {
   return (
     <div className="save-wrapper">
       <MovieCard title={title} director={director} metascore={metascore} stars={stars} />
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>
+        Save
+      </div>
     </div>
   );
 };
